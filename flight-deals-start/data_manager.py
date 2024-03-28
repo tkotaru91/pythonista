e4135d2e65b6618ccee46a3645dbc5123eb43e7b
tejaswi kotaru <tejaswikotaru@tejaswis-MacBook-Pro.local>
2024-03-27 22:42:34 -0700

    test

---
+++ flight-deals-start/data_manager.py
@@ -0,0 +1,36 @@
+import requests
+
+SHEETY_GET_API = "https://api.sheety.co/b715d9d72a4b58b1dd86fa26f57b1f03/flightDeals/prices/3"
+
+
+class DataManager:
+    #This class is responsible for talking to the Google Sheet.
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self, SHEETY_GET_API):
+        response = requests.get(SHEETY_GET_API)
+        response_data = response.json()
+        self.destination_data = response_data["prices"]
+        return self.destination_data
+
+    def update_destination(self):
+        for row in self.destination_data:
+            print(row)
+            new_data = {
+                "price": {
+                    "iataCode": row["iataCode"]
+                }
+            }
+
+            sheety_put_url = f"https://api.sheety.co/b715d9d72a4b58b1dd86fa26f57b1f03/flightDeals/prices/{row['id']}"
+            reponse = requests.put(sheety_put_url,json=new_data)
+
+        # reponse = requests.put(f"{SHEETY_GET_API}/{row['id']}",json=new_data)
+        print(reponse.text)
+
+
+
+
+
---
+++ flight-deals-start/main.py
@@ -0,0 +1,44 @@
+import requests
+from pprint import pprint
+from flight_search import FlightSearch
+from data_manager import DataManager
+from flight_search import FlightSearch
+from datetime import datetime,timedelta
+
+#headers = {"apikey": "TVEH29p1wb-GqbCkEwVm1vQnSlt7Wj8y"}
+
+#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+
+data_manager = DataManager()
+flight_search = FlightSearch()
+ORIGIN_CITY_IATA = 'LON'
+
+sheety_api = "https://api.sheety.co/b715d9d72a4b58b1dd86fa26f57b1f03/flightDeals/prices"
+response = requests.get(sheety_api)
+sheet_data =  (response.json())["prices"]
+# print(sheet_data)
+
+
+
+
+if sheet_data[0]["iataCode"] == "":
+    flight_search = FlightSearch()
+    for row in sheet_data:
+        row["iataCode"] = flight_search.getDestination(row["city"])
+    data_manager.update_destination()
+
+data_manager.destination_data = sheet_data
+tomorrow = datetime.now() + timedelta(days=1)
+six_months_from_today = datetime.now() + timedelta (days = 6*30)
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+       ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time = tomorrow,
+        to_time = six_months_from_today
+    )
+
+
+
